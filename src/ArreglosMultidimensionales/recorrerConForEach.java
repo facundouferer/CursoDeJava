af8ec37f13a1db9cd6c787edb317da133b578770
Facundo Uferer <juanfacundouf@gmail.com>
2019-08-15 00:16:35 -0300

    Recorrer Array bidimensionales con for-each

---
+++ src/ArreglosMultidimensionales/recorrerConForEach.java
@@ -0,0 +1,16 @@
+package ArreglosMultidimensionales;
+
+public class recorrerConForEach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] matriz = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+		
+		for(int [] fila:matriz) {
+			for(int elemento:fila) {
+				System.out.print(elemento+" ");
+			}
+			System.out.println("");
+			}
+		}
+}
