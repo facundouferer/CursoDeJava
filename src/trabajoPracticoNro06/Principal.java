0eba1f0f83e3be91bf5d0a3c5f83ae79f54a0d0c
Facundo Uferer <juanfacundouf@gmail.com>
2019-09-20 10:54:00 -0300

    clase 12

---
+++ src/trabajoPracticoNro06/Principal.java
@@ -0,0 +1,42 @@
+package trabajoPracticoNro06;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	private static Scanner scan;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		scan = new Scanner (System.in);
+        int  cantidad, longitud;
+		
+		System.out.println("cantidad de contraseñas:");
+		cantidad = scan.nextInt();
+        
+        System.out.println("longitud de la/las contraseña/as:");
+        longitud = scan.nextInt();
+        
+        Passwords passwords = new Passwords(cantidad, longitud);
+
+        System.out.println("CONTRASEÑAS");
+
+        for (int i = 0; i < passwords.getPasswords().length; i++) {
+        	
+            System.out.print(passwords.getPasswords()[i]);
+            System.out.print("--->");
+            
+            if(passwords.analizarSeguridad(passwords.getPasswords()[i])==1) {
+            	System.out.print("FUERTE");
+            }else {
+            	System.out.print("DEBIL");
+            }
+            
+            System.out.println("");
+            
+        }
+
+	}
+
+}
