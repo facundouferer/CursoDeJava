8fb2106105e92505b2049fb0567ff1dde253542d
juanfacundouf <juanfacundouf@gmail.com>
2019-10-17 21:47:53 -0300

    Trabajo Final Integrador Nro 01 Idividual RESUELTO

---
+++ src/tfiNro01/Empleados.java
@@ -0,0 +1,33 @@
+package tfiNro01;
+
+public class Empleados extends Personas{
+	
+	private double sueldo;
+	
+	public Empleados(String nombre, int edad) {
+		// TODO Auto-generated method stub
+		super.setNombre(nombre);
+		super.setEdad(edad);		
+	}
+
+	public void setSueldo(double sueldo) {
+		this.sueldo = sueldo;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Nombre: "+super.getNombre()+"\n"+
+		"Edad: "+super.getEdad()+"\n"+
+		"Tipo:"+this.getTipo()+"\n"+
+		"Sueldo: $"+this.sueldo+"\n"+
+		"------------------------\n";
+	}
+
+	@Override
+	public String getTipo() {
+		// TODO Auto-generated method stub
+		return "Empleado";
+	}
+
+}
---
+++ src/tfiNro01/ParaAcomodadores.java
@@ -0,0 +1,8 @@
+package tfiNro01;
+
+public interface ParaAcomodadores {
+	
+	public Salas getSala();
+	public void setSala(Salas sala);
+
+}
