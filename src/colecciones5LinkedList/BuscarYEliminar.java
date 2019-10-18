19212c1a548e1e7b7405b73497dea6d0c09ad08d
juanfacundouf <juanfacundouf@gmail.com>
2019-10-18 11:15:38 -0300

    en la clase 17

---
+++ src/colecciones5LinkedList/BuscarYEliminar.java
@@ -0,0 +1,33 @@
+package colecciones5LinkedList;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class BuscarYEliminar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedList<String> provincias = new LinkedList<String>();
+
+		provincias.add("Chaco");
+		provincias.add("Corrientes");
+		provincias.add("Misiones");
+		provincias.add("Buenos Aires");
+
+		System.out.println(provincias);
+
+		ListIterator<String> itProvincias = provincias.listIterator();
+
+		while (itProvincias.hasNext()) {
+			itProvincias.next();
+			if(itProvincias.next().equals("Corrientes")) {
+				itProvincias.remove();
+			}
+		}
+		
+		System.out.println(provincias);
+
+	}
+
+}
