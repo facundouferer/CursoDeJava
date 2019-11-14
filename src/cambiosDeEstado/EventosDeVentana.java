a57c17a6091490f19f0d109b30bf6a7590a4fc4a
Facundo Uferer <juanfacundouf@gmail.com>
2019-11-14 09:53:41 -0300

    cambios de estado

---
+++ src/cambiosDeEstado/EventosDeVentana.java
@@ -0,0 +1,20 @@
+package cambiosDeEstado;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
+
+class EventosDeVentana implements WindowStateListener{
+
+	@Override
+	public void windowStateChanged(WindowEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("La ventana ha cambiado de estado.");
+		//para ver que estado tenía
+		System.out.println(e.getNewState());
+	}
+	
+	
+	
+}
