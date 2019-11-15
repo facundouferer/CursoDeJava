2a1853104d1612ccbb433af0b69f88ceb03b7b27
Facundo Uferer <juanfacundouf@gmail.com>
2019-11-15 02:15:52 -0300

    eventos de raton

--- src/eventosDeTeclado01/EventosDeTeclado.java
+++ src/eventosDeTeclado01/EventosDeTeclado.java
@@ -9,7 +9,7 @@ class EventosDeTeclado implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent t) {
 		// TODO Auto-generated method stub
-		System.out.println(t.getKeyText(t.getKeyCode()));
+		System.out.println(t.getKeyText(t.getKeyCode())); //muestra la tecla apretada en formato de texto
 		if(t.getKeyCode()==KeyEvent.VK_DOWN) System.out.println("ABAJO");
 		if(t.getKeyCode()==KeyEvent.VK_UP) System.out.println("ARRIBA");
 		if(t.getKeyCode()==KeyEvent.VK_LEFT) System.out.println("IZQUIERDA");
