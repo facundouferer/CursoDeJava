f4c378431db66616b34fad9252e25437eda319de
Facundo Uferer <juanfacundouf@gmail.com>
2019-11-21 10:35:14 -0300

    evento de foco en clases

--- src/eventosDeFoco/EventoDeFoco.java
+++ src/eventosDeFoco/EventoDeFoco.java
@@ -47,10 +47,17 @@ class Panel_Marco_Foco extends JPanel {
 		@Override
 		public void focusGained(FocusEvent e) {
 			// TODO Auto-generated method stub
+			
 			if(e.getSource()==campoTexto01) {
 				System.out.println("EL CUADRO 1 GANO EL FOCO.");
-			}else {
+			}
+			
+			if(e.getSource()==campoTexto02) {
 				System.out.println("EL CUADRO 2 GANO EL FOCO.");
+			}
+			
+			if(e.getSource()==campoTexto03) {
+				System.out.println("EL CUADRO 3 GANO EL FOCO.");
 			}
 			
 		}
@@ -61,8 +68,14 @@ class Panel_Marco_Foco extends JPanel {
 			
 			if(e.getSource()==campoTexto01) {
 				System.out.println("EL CUADRO 1 PERDIÓ EL FOCO.");
-			}else {
+			}
+			
+			if(e.getSource()==campoTexto02) {
 				System.out.println("EL CUADRO 2 PERDIÓ EL FOCO.");
+			}
+			
+			if(e.getSource()==campoTexto03) {
+				System.out.println("EL CUADRO 3 PERDIÓ EL FOCO.");
 			}
 			
 		}
@@ -77,25 +90,31 @@ class Panel_Marco_Foco extends JPanel {
 		//creamos los campos de texto
 		campoTexto01 = new JTextField();
 		campoTexto02 = new JTextField();
+		campoTexto03 = new JTextField();
 		
 		//posicionamos los componentes en el panel
 		campoTexto01.setBounds(120, 10, 150, 30);
 		campoTexto02.setBounds(120, 50, 150, 30);
+		campoTexto03.setBounds(120, 90, 150, 30);
 		
 		//con esto agregamos los campos de texto
 		add(campoTexto01);
 		add(campoTexto02);
+		add(campoTexto03);
 		
 		//creamos las etiquetas para los cuadros de texto
 		etiqueta01 = new JLabel("CUADRO 1");
-		etiqueta02 = new JLabel("CUADRO 2");		
+		etiqueta02 = new JLabel("CUADRO 2");
+		etiqueta03 = new JLabel("CUADRO 3");
 		
 		etiqueta01.setBounds(40, 10, 150, 20);
 		etiqueta02.setBounds(40, 50, 150, 20);
+		etiqueta03.setBounds(40, 90, 150, 20);
 		
 		//agrego las etiquetas
 		add(etiqueta01);
 		add(etiqueta02);
+		add(etiqueta03);
 		
 		
 		//colocamos los componentes a la escucha
@@ -105,10 +124,12 @@ class Panel_Marco_Foco extends JPanel {
 		
 		campoTexto02.addFocusListener(foco);
 		
+		campoTexto03.addFocusListener(foco);
+
 	}
 	
-	private JTextField campoTexto01, campoTexto02; 
-	private JLabel etiqueta01, etiqueta02; 
+	private JTextField campoTexto01, campoTexto02, campoTexto03; 
+	private JLabel etiqueta01, etiqueta02, etiqueta03; 
 	
 }
 
