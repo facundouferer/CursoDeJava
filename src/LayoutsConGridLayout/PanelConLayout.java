d10313bbd9e5f847765184d9d123400e90cd5e6e
Facundo Uferer <juanfacundouf@gmail.com>
2019-11-22 09:32:34 -0300

    GridLayout

---
+++ src/LayoutsConGridLayout/PanelConLayout.java
@@ -0,0 +1,27 @@
+package LayoutsConGridLayout;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+class PanelConLayout extends JPanel{
+	
+	
+	public PanelConLayout() {
+		
+		/*
+		 * aquí es donde indicamos el tipo 
+		 * de layouts 
+		 */
+		
+		setLayout(new GridLayout(2,2));
+		add(new JButton("uno"));
+		add(new JButton("dos"));
+		add(new JButton("tres"));
+		add(new JButton("cuatro"));
+	}
+
+}
