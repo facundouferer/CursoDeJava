980e66ecd825a67ab974590759230546e2b633b9
Facundo Uferer <juanfacundouf@gmail.com>
2019-11-28 15:36:10 -0300

    ejemplo de colecciones con iteradores

---
+++ src/calculadora/PanelDisplay.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+class PanelDisplay extends JPanel{
+	
+	public PanelDisplay() {
+		add(new JButton("----"));
+	}
+
+}
