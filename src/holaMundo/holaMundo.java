e3b214881dc9bef65a55ac27c520d33f98ac2e72
juanfacundouf <juanfacundouf@gmail.com>
2020-02-17 00:28:26 -0300

    inicio del anio

---
+++ src/holaMundo/holaMundo.java
@@ -0,0 +1,23 @@
+package holaMundo;
+
+public class holaMundo {
+
+	public static void main(String[] args) {
+		
+		// método para escribir HOLA MUNDO por consola
+
+		String texto = "Hola mundo";
+
+		System.out.println(texto);
+
+		/*
+		 * Este es un comentario de varias líneas.
+		 */
+
+		/**
+		 * Esto es un javadoc o comentario de documentación
+		 */
+
+	}
+
+}
