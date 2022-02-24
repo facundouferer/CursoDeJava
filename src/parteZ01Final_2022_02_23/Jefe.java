0c67ca30713423f5d6a1b262e89bd328380d9482
Facundo Uferer <juanfacundouf@gmail.com>
2022-02-24 10:12:06 -0300

    Examen Final de 23-02-2022

---
+++ src/parteZ01Final_2022_02_23/Jefe.java
@@ -0,0 +1,28 @@
+package parteZ01Final_2022_02_23;
+
+public class Jefe extends Persona{
+
+    private double porcentaje;
+
+    Jefe(String nombre, String apellido, int dni, double sueldo, double porcentaje) {
+        super(nombre, apellido, dni, sueldo);
+        this.porcentaje = porcentaje;
+    }
+
+    @Override
+    public void subirSueldo(double incremento) {
+        double monto = super.getSueldoBasico() + incremento+ super.getSueldoBasico() * porcentaje;
+        super.setSueldoBasico(monto);
+    }
+
+    @Override
+    double verSueldo() {
+        double monto = super.getSueldoBasico() + super.getSueldoBasico() * porcentaje;
+        return monto;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " Porcentaje: " + this.porcentaje;
+    }
+}
