5b53b1a019e891bb081c467bc32a1de22c43a49f
facundouferer <juanfacundouf@gmail.com>
2023-03-31 22:07:49 -0300

    ejercicios del 1 al 12

--- src/programacion1/ej02PrecioBoleto.java
+++ src/programacion1/ej02PrecioBoleto.java
@@ -11,10 +11,11 @@ public class ej02PrecioBoleto {
         Scanner input = new Scanner(System.in); // Creamos una instancia de la clase Scanner para leer la entrada del usuario
 
         System.out.println("Ingrese el número de kilómetros a recorrer: "); // Solicitamos al usuario que ingrese el número de kilómetros
-        int km = input.nextInt(); // Leemos el número de kilómetros ingresado por el usuario
+        double km = input.nextDouble(); // Leemos el número de kilómetros ingresado por el usuario
 
         double precio = km * 10.50; // Calculamos el precio del boleto multiplicando el número de kilómetros por el precio por kilómetro
 
-        System.out.println("El precio del boleto es de: Bs/." + precio); // Mostramos el precio del boleto al usuario
+        System.out.println("El precio del boleto es de: $ " + precio); // Mostramos el precio del boleto al usuario
+        input.close();
     }
 }
