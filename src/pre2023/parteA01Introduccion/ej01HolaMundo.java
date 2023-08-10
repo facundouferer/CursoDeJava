9ecc598d15f51f5d9b5f96cccc9d76c4121de74a
facundouferer <juanfacundouf@gmail.com>
2023-08-10 11:39:24 -0300

    prueba

--- src/pre2023/parteA01Introduccion/ej01HolaMundo.java
+++ src/pre2023/parteA01Introduccion/ej01HolaMundo.java
@@ -4,16 +4,16 @@ public class ej01HolaMundo {
 
     public static void main(String[] args) {
 
-        // método para escribir HOLA MUNDO por consola
+        // mï¿½todo para escribir HOLA MUNDO por consola
 
-        System.out.println("Hola Mundo TODO OK");
+        System.out.println("Hola Mundo TODO OK!");
 
         /*
-         * Este es un comentario de varias líneas.
+         * Este es un comentario de varias lï¿½neas.
          */
 
         /**
-         * Esto es un javadoc o comentario de documentación
+         * Esto es un javadoc o comentario de documentaciï¿½n
          */
 
     }
