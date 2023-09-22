c097bc0874bd890984b36547356c101a9b75bc02
facundouferer <juanfacundouf@gmail.com>
2023-09-22 10:14:02 -0300

    mejoras BD

--- src/CursoJava/BasesDeDatos/ConsultaAgregarEstudiante.java
+++ src/CursoJava/BasesDeDatos/ConsultaAgregarEstudiante.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class ConsultaAgregarEstudiante {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/Universidad";
+        String url = "jdbc:mysql://localhost:3306/universidad";
         String user = "root";
         String pass = "";
 
