89d566fe3c6b42df32d20756e2da2a32f6731e78
Facundo Uferer <juanfacundouf@gmail.com>
2025-08-18 19:58:10 -0300

    Clase 2

--- src/CursoJava/Inicio/Operadores.java
+++ src/CursoJava/Inicio/Operadores.java
@@ -1,9 +1,33 @@
 package CursoJava.Inicio;
 
+import java.util.Scanner;
+
 public class Operadores {
   public static void main(String[] args) {
-    int numero1 = 10;
-    // Incrementa en 1 el valor de la variable
-    System.out.println(3 + numero1);
+    int nro1, nro2;
+    Scanner entrada = new Scanner(System.in);
+    System.out.print("Ingrese el primer número: ");
+    nro1 = entrada.nextInt();
+    System.out.print("Ingrese el segundo número: ");
+    nro2 = entrada.nextInt();
+    dividir(nro1, nro2);
+    entrada.close();
+  }
+
+  public static void sumar(int nro1, int nro2) {
+    System.out.println(nro1 + nro2);
+  }
+
+  public static void restar(int nro1, int nro2) {
+    System.out.println(nro1 - nro2);
+  }
+
+  public static void multiplicar(int nro1, int nro2) {
+    System.out.println(nro1 * nro2);
+  }
+
+  public static void dividir(double nro1, double nro2) {
+    double resultado = nro1 / nro2;
+    System.out.println(resultado);
   }
 }
---
+++ src/Practica/Operadores.java
@@ -0,0 +1,103 @@
+package Practica;
+
+import java.util.Scanner;
+
+class Menu {
+  public void mostrarMenu() {
+    System.out.println(
+        "Seleccione la operación a realizar\n 1. Sumar\n 2. Restar\n 3. Multiplicar\n 4. Dividir\n 0. Salir");
+  }
+}
+
+class Calculadora {
+
+  private int nro1, nro2;
+
+  public Calculadora(int nro1, int nro2) {
+    this.nro1 = nro1;
+    this.nro2 = nro2;
+  }
+
+  public int sumar() {
+    return this.nro1 + this.nro2;
+  }
+
+  public int restar() {
+    int resultado = this.nro1 - this.nro2;
+    String mensaje = (this.nro1 > this.nro2) ? ("Resultado:" + resultado) : "El resultado es negativo";
+    System.out.println(mensaje);
+    return resultado;
+  }
+
+  public int multiplicar() {
+    return this.nro1 * this.nro2;
+  }
+
+  public double dividir() {
+    if ((double) this.nro1 == 0 && (double) this.nro2 == 0) {
+      System.out.println("El resultado es indeterminado");
+    } else {
+      if (this.nro2 == 0) {
+        System.out.println("No se puede dividir por cero");
+      } else {
+        return (double) this.nro1 / (double) this.nro2;
+      }
+    }
+    return 0;
+  }
+
+  public int getNro1() {
+    return nro1;
+  }
+
+  public int getNro2() {
+    return nro2;
+  }
+
+}
+
+public class Operadores {
+  public static void main(String[] args) {
+    int nro1 = 0;
+    int nro2 = 0;
+    int opcion = 0;
+    Scanner entrada = new Scanner(System.in);
+    Menu menuObjeto = new Menu();
+    do {
+      menuObjeto.mostrarMenu();
+      opcion = entrada.nextInt();
+      if (opcion != 0) {
+        System.out.print("Ingrese el primer número: ");
+        nro1 = entrada.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        nro2 = entrada.nextInt();
+      }
+      Calculadora calculadoraObjeto = new Calculadora(nro1, nro2);
+      switch (opcion) {
+        case 1:
+          System.out.println(
+              calculadoraObjeto.getNro1() + "+" + calculadoraObjeto.getNro2() + " = " + calculadoraObjeto.sumar());
+          break;
+        case 2:
+          System.out.println(
+              calculadoraObjeto.getNro1() + "-" + calculadoraObjeto.getNro2() + " = " + calculadoraObjeto.restar());
+          break;
+        case 3:
+          System.out.println(calculadoraObjeto.getNro1() + "*" + calculadoraObjeto.getNro2() + " = "
+              + calculadoraObjeto.multiplicar());
+          break;
+        case 4:
+          System.out.println(
+              calculadoraObjeto.getNro1() + "/" + calculadoraObjeto.getNro2() + " = " + calculadoraObjeto.dividir());
+          break;
+        case 0:
+          System.out.println("FIN");
+          break;
+        default:
+          System.out.println("Opción no válida");
+      }
+    } while (opcion != 0);
+    entrada.close();
+  }
+
+}
