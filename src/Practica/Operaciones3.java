c60498bf05a5b9b1fac957e6e5ab344627ab2d94
Facundo Uferer <juanfacundouf@gmail.com>
2025-09-01 15:36:31 -0300

    clase 2 com 2

---
+++ src/Practica/Operaciones3.java
@@ -0,0 +1,77 @@
+package Practica;
+
+import java.util.Scanner;
+
+class Auto {
+  private String color;
+  private String marca;
+  private int anio;
+  private String traccion;
+
+  public Auto(String color, String marca, int anio, String traccion) {
+    this.color = color;
+    this.marca = marca;
+    this.anio = anio;
+    this.traccion = traccion;
+  }
+
+  public String toString() {
+    // String mensaje = String.format("Color: %s\nMarca: %s\nAño: %d\nTracción: %s",
+    // color, marca, anio, traccion);
+    return "--------------------------\nColor: " + color + "\nMarca: " + marca + "\nAño: " + anio + "\nTracción: "
+        + traccion;
+  }
+
+}
+
+class Garage {
+
+  Scanner teclado = new Scanner(System.in);
+  Auto[] estacionamiento = new Auto[3];
+
+  Garage(Scanner teclado) {
+    this.teclado = teclado;
+  }
+
+  public void agregarAuto(int lugar) {
+
+    System.out.println("ingrese el Color");
+    String color = teclado.nextLine();
+
+    System.out.println("ingrese la Marca");
+    String marca = teclado.nextLine();
+
+    System.out.println("ingrese el Año");
+    int anio = teclado.nextInt();
+
+    teclado.nextLine(); // Limpiar el buffer
+
+    System.out.println("ingrese la Tracción");
+    String traccion = teclado.nextLine();
+
+    Auto auto = new Auto(color, marca, anio, traccion);
+    this.estacionamiento[lugar] = auto;
+  }
+
+  public void listarAutos() {
+    for (Auto auto : estacionamiento) {
+      if (auto != null) {
+        System.out.println(auto.toString());
+      }
+    }
+
+  }
+}
+
+class Operaciones3 {
+
+  public static void main(String[] args) {
+
+    Scanner teclado = new Scanner(System.in);
+    Garage garage = new Garage(teclado);
+    garage.agregarAuto(0);
+    garage.listarAutos();
+    teclado.close();
+  }
+
+}
---
+++ src/Practica/Operadores2.java
@@ -0,0 +1,54 @@
+package Practica;
+
+class Matematica {
+  private int a, b;
+
+  Matematica(int nro1, int nro2) {
+    this.a = nro1;
+    this.b = nro2;
+  }
+
+  public int sumar() {
+    return a + b;
+  }
+
+  public int restar() {
+    return a - b;
+  }
+
+  public int multiplicar() {
+    return a * b;
+  }
+
+  public int dividir() {
+    int resultdo = 0;
+    if (b != 0) {
+      resultdo = a / b;
+    } else {
+      System.out.println("Error: Division por cero");
+    }
+    return resultdo;
+  }
+
+  public void setA(int nro1) {
+    this.a = nro1;
+  }
+
+  public int getA() {
+    return a;
+  }
+
+}
+
+class Operadores2 {
+
+  public static void main(String[] args) { // acá inicia todo
+    int nro1 = 5;
+    int nro2 = 5;
+
+    Matematica mat = new Matematica(nro1, nro2);
+
+    System.out.println("El resultado es: " + mat.sumar());
+  }// acá termina todo
+
+}
