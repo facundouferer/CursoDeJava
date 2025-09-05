b31356456112022cd4b8dc7756e05633bdf47c53
Facundo Uferer <juanfacundouf@gmail.com>
2025-09-05 11:04:23 -0300

    clase 5

--- src/Practica/Practica04.java
+++ src/Practica/Practica04.java
@@ -20,6 +20,10 @@ class FiguraGeometrica {
     this.nombre = "";
   }
 
+  public String getNombre() {
+    return nombre;
+  }
+
   // sobreescritura del método de la clase Object
   public String toString() {
     return "-----------------------\nNombre: " + nombre + "\nLados: " + nroLados + "\nAngulos: " + cantAngulos;
@@ -28,6 +32,7 @@ class FiguraGeometrica {
 
 class ColeccionDeFiguras {
   Scanner teclado = new Scanner(System.in);
+  int ultimoLuegar;
   FiguraGeometrica[] figuras = new FiguraGeometrica[5];
 
   /**
@@ -39,6 +44,7 @@ class ColeccionDeFiguras {
    */
   public ColeccionDeFiguras(Scanner teclado) {
     this.teclado = teclado;
+    this.ultimoLuegar = 0;
   }
 
   public void agregarFigura() {
@@ -50,19 +56,80 @@ class ColeccionDeFiguras {
     System.out.println("Ingrese la cantidad de angulos:");
     int angulos = teclado.nextInt();
     FiguraGeometrica figura = new FiguraGeometrica(lados, angulos, nombre);
-    System.out.println("Ingrese el lugar donde desea agregar la figura (0-4):");
-    int lugar = teclado.nextInt();
-    figuras[lugar] = figura;
+    figuras[ultimoLuegar] = figura;
+    ultimoLuegar++;
     teclado.nextLine(); // Limpiar el buffer para evitar problemas con nextLine()
   }
 
   public void listarFiguras() {
+    System.out.println("-------------\nListado de figuras:");
     for (FiguraGeometrica figura : this.figuras) {
       if (figura != null) {
         System.out.println(figura.toString());
       }
     }
   }
+
+  public void modificarFiguraPosicion() {
+    System.out.println("Ingrese el nombre de la figura:");
+    String nombre = teclado.nextLine();
+    System.out.println("Ingrese la cantidad de lados:");
+    int lados = teclado.nextInt();
+    System.out.println("Ingrese la cantidad de angulos:");
+    int angulos = teclado.nextInt();
+    FiguraGeometrica figura = new FiguraGeometrica(lados, angulos, nombre);
+    System.out.println("Ingrese la posición donde desea modificar la figura:");
+    int posicion = teclado.nextInt();
+    this.figuras[posicion] = figura;
+  }
+
+  public void modificarFiguraPosicion(int posicion) {
+    System.out.println("Ingrese el nombre de la figura:");
+    String nombre = teclado.nextLine();
+    System.out.println("Ingrese la cantidad de lados:");
+    int lados = teclado.nextInt();
+    System.out.println("Ingrese la cantidad de angulos:");
+    int angulos = teclado.nextInt();
+    FiguraGeometrica figura = new FiguraGeometrica(lados, angulos, nombre);
+    this.figuras[posicion] = figura;
+  }
+
+  public FiguraGeometrica obtenerFigura(String nombreBuscado) {
+    for (FiguraGeometrica figura : this.figuras) {
+      if (figura != null && figura.getNombre().equalsIgnoreCase(nombreBuscado)) {
+        return figura;
+      }
+    }
+    return null;
+  }
+
+  public int obtenerPosicionFigura(String nombreBuscado) {
+    for (int i = 0; i < this.figuras.length; i++) {
+      if (this.figuras[i] != null && this.figuras[i].getNombre().equalsIgnoreCase(nombreBuscado)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public void modificarFiguraNombre(String nombreBuscado) {
+    int posicion = obtenerPosicionFigura(nombreBuscado);
+    if (posicion != -1) {
+      modificarFiguraPosicion(posicion);
+    } else {
+      System.out.println("No se encontró la figura con el nombre: " + nombreBuscado);
+    }
+  }
+
+  public void eliminarFiguraNombre(String nombreBuscado) {
+    int posicion = obtenerPosicionFigura(nombreBuscado);
+    if (posicion != -1) {
+      this.figuras[posicion] = null;
+    } else {
+      System.out.println("No se encontró la figura con el nombre: " + nombreBuscado);
+    }
+  }
+
 }
 
 /**
@@ -77,6 +144,11 @@ public class Practica04 {
     ColeccionDeFiguras figurasGuardadas = new ColeccionDeFiguras(teclado);
     figurasGuardadas.agregarFigura();
     figurasGuardadas.agregarFigura();
+    figurasGuardadas.agregarFigura();
+    figurasGuardadas.listarFiguras();
+    System.out.println("Ingrese el nombre de la figura a eliminar:");
+    String nombreEliminar = teclado.nextLine();
+    figurasGuardadas.eliminarFiguraNombre(nombreEliminar);
     figurasGuardadas.listarFiguras();
 
     teclado.close();
