bb00db181f7a3b18bfe98a4760df59c8c0653aad
Facundo Uferer <juanfacundouf@gmail.com>
2026-08-25 21:05:32 -0300

    hasta acá por hoy

--- src/CursoJava/ArreglosDeObjetos/GestorDeProductos.java
+++ src/CursoJava/ArreglosDeObjetos/GestorDeProductos.java
@@ -6,16 +6,23 @@ class GestorDeProductos {
         
         ManejoDeStock stock = new ManejoDeStock();
 
-        Producto producto1 = new Producto("Producto 1", 10.99, 100);
-        Producto producto2 = new Producto("Producto 2", 5.49, 50);
-        Producto producto3 = new Producto("Producto 3", 15.75, 75);
+        Producto producto1 = new Producto("tomate", 10.99, 100);
+        Producto producto2 = new Producto("lechuga", 5.49, 50);
+        Producto producto3 = new Producto("cebolla", 15.75, 75);
 
-        stock.agregarProducto(producto1, 0);
-        stock.agregarProducto(producto2, 1);
-        stock.agregarProducto(producto3, 2);
+        stock.agregarProducto(producto1);
+        stock.agregarProducto(producto2);
+        stock.agregarProducto(producto3);
 
         System.out.println("Lista de productos:");
+        stock.listarProducto();
+
+        stock.eliminarProducto("lechuga");
+        System.out.println("Después de eliminar el producto en el índice 1:");
         System.out.println(stock);
+
+        stock.venderProducto("cebolla", 80);
+        stock.listarProducto();
 
     }
     
@@ -27,16 +34,56 @@ class ManejoDeStock{
 
     //constructor
     public ManejoDeStock(){ 
-        this.productos = new Producto[1];
+        this.productos = new Producto[0];
     }
 
-    public void agregarProducto(Producto product, int indice){
-        this.productos[indice] = product;
+    public void agregarProducto(Producto productoNuevo){
+        Producto [] nuevosProductos = new Producto[productos.length + 1];
+        nuevosProductos[nuevosProductos.length - 1] = productoNuevo;
+        for (int i = 0; i < productos.length; i++) {
+            nuevosProductos[i] = productos[i];
+        }
+        this.productos = nuevosProductos;
     }
 
     public void eliminarProducto(int indice){
-        this.productos[indice] = null;
+            Producto [] nuevosProductos = new Producto[productos.length - 1];
+            for (int i = 0, j = 0; i < productos.length; i++) {
+                if(i != indice){
+                    nuevosProductos[j++] = productos[i];
+                }
+            }
+        this.productos = nuevosProductos;
     }
+
+    public void venderProducto(String nombre, int cantidad){
+        int indice = this.verID(nombre);
+        if(indice != -1){
+            if(productos[indice].getStock() >= cantidad){
+                productos[indice].setStock(cantidad);
+            }else{
+                System.out.println("No hay suficiente stock para vender " + cantidad + " unidades de " + nombre);
+            }
+        } else{
+            System.out.println("El producto " + nombre + " no se encuentra en el stock.");
+        }
+    }
+
+    public int verID(String nombre){
+        for (int i = 0; i < productos.length; i++) {
+            if(productos[i].getNombre().equals(nombre)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void eliminarProducto(String nombre){
+        int indice = this.verID(nombre);
+        this.eliminarProducto(indice); 
+    }
+
+
 
     @Override
     public String toString(){
@@ -66,6 +113,18 @@ class Producto{
         this.nombre = nombre;
         this.precio = precio;
         this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setStock(int cantidad) {
+        this.stock -= cantidad;
+    }
+
+    public int getStock() {
+        return stock;
     }
 
     @Override
