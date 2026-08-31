d49d61588e8f23aaeaa8aca44c4a96184fabe996
nestorjgomez <39187176+nestorjgomez@users.noreply.github.com>
2026-08-31 16:08:24 -0300

    Uso del Panel de Control

--- src/CursoJava/ArreglosDeObjetos/GestorDePersonal.java
+++ src/CursoJava/ArreglosDeObjetos/GestorDePersonal.java
@@ -1,21 +1,57 @@
+import java.util.Scanner;
+
 package CursoJava.ArreglosDeObjetos;
 
- class GestorDePersonal {
+class GestorDePersonal {
 
     public static void main(String[] args) {
         // Crear un arreglo de objetos de tipo Persona
 
         Agenda listaDePersonas = new Agenda();
 
-        listaDePersonas.agregarPersona(new Persona("Carlitos", 12345678));
-        listaDePersonas.agregarPersona(new Persona("Maria", 23456721));
-        listaDePersonas.agregarPersona(new Persona("Juana", 23456789));
+        Scanner scanner = new Scanner(System.in);
 
-        listaDePersonas.listarPersonas();
-        listaDePersonas.eliminarPersona("Carlitos");
-        listaDePersonas.listarPersonas();
+        int opcion;
+
+        do {
+            System.out.println("\n===== MENÚ =====");
+            System.out.println("1) Agregar Persona");
+            System.out.println("2) Listar Personas");
+            System.out.println("3) Salir");
+            System.out.print("Seleccione una opción: ");
+
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.print("Ingrese el nombre: ");
+                    String nombre = scanner.next();
+
+                    System.out.print("Ingrese el DNI: ");
+                    int dni = scanner.nextInt();
+
+                    listaDePersonas.agregarPersona(new Persona(nombre, dni));
+
+                    System.out.println("Persona agregada correctamente.");
+                    break;
+
+                case 2:
+                    listaDePersonas.listarPersonas();
+                    break;
+
+                case 3:
+                    System.out.println("Saliendo del programa...");
+                    break;
+
+                default:
+                    System.out.println("Opción incorrecta. Solo se pueden usar las opciones 1 y 2.");
+            }
+
+        } while (opcion != 3);
+
+        scanner.close();
     }
-
 }
 
 class PenelDeControl{
@@ -34,7 +70,7 @@ class PenelDeControl{
 class Agenda{
 
     Persona [] personas;
-    
+
     public Agenda(){
         this.personas = new Persona[0];
     }
@@ -59,19 +95,19 @@ class Agenda{
 
     }
 
- public void eliminarPersona(int indice){
-    Persona [] nuevaLista = new Persona[personas.length - 1];
-    System.arraycopy(personas, 0, nuevaLista, 0, indice); 
-    System.arraycopy(personas, indice + 1, nuevaLista, indice, nuevaLista.length - indice);
+    public void eliminarPersona(int indice){
+        Persona [] nuevaLista = new Persona[personas.length - 1];
+        System.arraycopy(personas, 0, nuevaLista, 0, indice);
+        System.arraycopy(personas, indice + 1, nuevaLista, indice, nuevaLista.length - indice);
 
-    personas = nuevaLista;
- }
+        personas = nuevaLista;
+    }
 
- public void editarPersona(int indice, String nuevaPersona, int nuevoDni) {
-    personas[indice] = new Persona(nuevaPersona, nuevoDni);
- }
+    public void editarPersona(int indice, String nuevaPersona, int nuevoDni) {
+        personas[indice] = new Persona(nuevaPersona, nuevoDni);
+    }
 
-  public int verIndice(Persona nombreBuscado) {
+    public int verIndice(Persona nombreBuscado) {
         int indice = 0;
         for (Persona persona : personas) {
             if(persona.equals(nombreBuscado)) {
@@ -108,4 +144,3 @@ class Persona {
         return this.nombre + " (" + dni + ")";
     }
 }
-
